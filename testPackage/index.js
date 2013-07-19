8fd6e9bf5bf67464c61bf2fb2da326acc5886781
garudlaksha2 <garud.laksha.2@gmail.com>
2013-07-20 00:52:21 +0530

    more methods

--- testPackage/index.js
+++ testPackage/index.js
@@ -1,5 +1,20 @@
+reportObj = {'data':'1',
+             'data2':'2'}
+
 var testfun = function (){
   console.log("in package testing");
 }
 
+var getToolInfo = function (callBack){
+  console.log("in get info");
+  callBack(reportObj);
+}
+
+var runTool = function(scanID, callBack){
+  console.log("run tool");
+  callBack("ok");
+}
+
 exports.testfun = testfun;
+exports.getToolInfo = getToolInfo;
+exports.runTool = runTool;
